cc79476b600227c245411196dafa2d89514eed38
namankhurpia <naman.khurpia2016@vitalum.ac.in>
2023-05-07 14:22:02 -0400

    adding vault integration code to fetch credentials

--- src/main/java/com/rabbitMq/rabbitmqscheduler/RabbitmqSchedulerApplication.java
+++ src/main/java/com/rabbitMq/rabbitmqscheduler/RabbitmqSchedulerApplication.java
@@ -1,16 +1,23 @@
 package com.rabbitMq.rabbitmqscheduler;
 
+import com.rabbitMq.rabbitmqscheduler.DTO.Properties;
+import com.rabbitmq.client.Command;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
 @EnableEurekaClient
-public class RabbitmqSchedulerApplication {
+@EnableConfigurationProperties({Properties.class})
+public class RabbitmqSchedulerApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitmqSchedulerApplication.class, args);
+
 	}
 
 }
